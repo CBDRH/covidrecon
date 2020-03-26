363373ea53dc8e91d0f6f1cd218f8ab7715e3675
Tim Churches <tim.churches@gmail.com>
2020-03-26 16:03:24 +1100

    Fix and document China data patching.

--- R/patch-china-data.R
+++ R/patch-china-data.R
@@ -2,19 +2,27 @@
 #'
 #' In Mid February, China changed their case definition, from
 #'   RT-PCR-swab-positive to test-positive or clinical signs of viral pneumonia.
-#'   This led to a large spike not representative of cases. some further thoughts: \url{https://www.worldometers.info/coronavirus/how-to-interpret-feb-12-case-surge/}
-#'   This changes the cases from date: 13/2/2020 to 1820 cases (from 15141),
-#'   and on date: 4/2/2020 to 1995 cases (from 3237).
+#'   This led to a large spike not representative of cases.
+#'   some further thoughts:
+#'    \url{https://www.worldometers.info/coronavirus/how-to-interpret-feb-12-case-surge/}
+#'   This changes the cases on the dates shown below to the verified lab-confirmed only counts
+#'   available at
+#'   https://en.wikipedia.org/w/index.php?title=Template:2019–20_coronavirus_pandemic_data/China_medical_cases_by_province&oldid=946973173
+#'   Note that the EuroCDC data has dates a day later than the wikipedia data for China
+#'   due to time zone differences. This adjustment is only valid for EuroCDC data, for the JHU
+#'   data the dates for the adjustments need to be shifted back on day.
 #'
 #' @param data data pulled by
 #' @note internal
 #'
-#' @return data.frame wtih adjusted cases
+#' @return data.frame with adjusted cases
 patch_china_data <- function(data){
   dplyr::mutate(data,
                 cases = dplyr::case_when(
                   geo_id == "CN" & year == 2020 & month == 2 & day == 13 ~ 1820,
-                  geo_id == "CN" & year == 2020 & month == 2 & day == 4 ~ 1995,
+                  geo_id == "CN" & year == 2020 & month == 2 & day == 14 ~ 1995,
+                  geo_id == "CN" & year == 2020 & month == 2 & day == 15 ~ 1503,
+                  geo_id == "CN" & year == 2020 & month == 2 & day == 16 ~ 1121,
                   TRUE ~ cases
                 ))
 }
